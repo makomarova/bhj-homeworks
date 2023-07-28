4443458af41793ef78e85dbd662ce44fe5b0a4d8
Rita Komarova <makomarova91@gmail.com>
2023-07-28 14:27:43 +0300

    Исправление preloader json

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,10 +1,11 @@
 const loader =document.querySelector('.loader ');
 const items = document.getElementById('items');
 const xhr = new XMLHttpRequest();
+xhr.responseType = 'json';
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
 xhr.addEventListener('load', function() {
         loader.classList.remove('loader_active');
-        let response = JSON.parse(xhr.responseText).response.Valute;
+        let response = xhr.response.response.Valute;
         console.log(response);
         for (var key in response)  {
             let html  = `<div class="item">
