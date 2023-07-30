4f36ae92ee74e018b5fc32047d3d87a8d2ff46f9
Rita Komarova <makomarova91@gmail.com>
2023-07-30 16:19:25 +0300

    Восьмой урок, задача 2 исправление по куки2

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,16 +1,7 @@
 const modal = document.getElementById('subscribe-modal');
 const modalClose = document.querySelector('.modal__close');
 
-function getCookie(name){
-    const pairs = document.cookie.split('; ');
-    const cookie = pairs.find(p => p.startsWith(name+'='));
-    if(cookie === undefined) {
-        return null;
-    }
-    return cookie.substring(name.lenght + 1);
-}
-
-if((getCookie('modalClose') === null) || (getCookie('modalClose') !== 'true')){
+if(!document.cookie.includes('modalClose=true')){
     modal.classList.add('modal_active');
 }
 
